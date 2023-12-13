6e3381653a9301c17ffe27bc9a32a933e12847b6
Szymon Smagowski <smagowski.szymon@gmail.com>
2023-12-13 01:59:41 +0100

    started_upgrading_update_endpoint

---
+++ web_scraping_additional_monthly.py
@@ -0,0 +1,113 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+from datetime import datetime, timedelta
+
+def Start_day_filter(driver):
+    data_od = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, '_VisioToolbarPortlet_WAR_visioneoportlet_data_od'))
+    )
+    data_od.click()
+
+    date_start = datetime.now() - timedelta(days=30)
+
+    year = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'ui-datepicker-year'))
+    )
+    year.click()
+    year_pick = year.find_element(By.CSS_SELECTOR, f'option[value="{str(date_start.year)}"]')
+    year_pick.click()
+
+    month = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'ui-datepicker-month'))
+    )
+    month.click()
+    month_pick = month.find_element(By.CSS_SELECTOR, f'option[value="{str(date_start.month-1)}"]')
+    month_pick.click()
+
+    day = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//a[@class='ui-state-default'][text()='{str(date_start.day)}']"))
+    )
+    day.click()
+    time.sleep(3)
+    return 
+
+
+def End_day_filter(driver):
+    data_do = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, '_VisioToolbarPortlet_WAR_visioneoportlet_data_do'))
+    )
+    data_do.click()
+
+    day_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//button[text()='Dziś']"))
+    )
+    day_button.click()
+    time.sleep(3)
+    return 
+
+
+def Change_latest_filename(directory, new_name):
+    try:
+        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+
+        # Sortujemy pliki według daty modyfikacji
+        files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)), reverse=True)
+
+        if files:
+            latest_file = files[0]
+            new_path = os.path.join(directory, new_name)
+            os.rename(os.path.join(directory, latest_file), new_path)
+
+            print(f"Najnowszy plik został pomyślnie zmieniony na {new_name}")
+
+        else:
+            print("Brak plików w katalogu.")
+
+    except Exception as e:
+        print("Wystąpił błąd podczas zmiany nazwy pobranego pliku:", str(e))
+    return
+
+
+def Change_filter_and_download(driver):
+    eksport_za_okres = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[title="Eksport za okres"]'))
+    )
+    eksport_za_okres.click()
+
+    Start_day_filter(driver)
+    End_day_filter(driver)
+
+    csv_link = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[title="Eksport do CSV"]'))
+    )
+    csv_link.click()
+    time.sleep(20)
+    return
+
+
+def Start_additional_data_scraping(nowa_nazwa_pliku = 'dane_kse.csv'):
+    try:
+
+        webdriver_path = 'edge_driver\\msedgedriver.exe'
+        url = 'https://www.pse.pl/dane-systemowe/plany-pracy-kse/plan-koordynacyjny-5-letni/wielkosci-podstawowe'
+
+
+        download_path = os.getcwd()
+        download_path = os.path.join(download_path, 'data')
+
+        edge_options = webdriver.EdgeOptions()
+        edge_options.add_experimental_option('prefs', {'download.default_directory': download_path})
+
+        driver = webdriver.Edge(options=edge_options)
+        driver.get(url)
+
+        Change_filter_and_download(driver)
+        Change_latest_filename(download_path, nowa_nazwa_pliku)
+    except Exception as e:
+        print("Wystąpił błąd podczas scrapowania dodatkowych danych:", str(e))
+
+    return
---
+++ web_scraping_price_monthly.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from datetime import datetime, timedelta
+import pandas as pd
+
+def Correct_date_click(driver):
+    calendar_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CLASS_NAME, "icon-calendar"))
+    )
+
+    calendar_button.click()
+
+    calendar = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "table-condensed"))
+    )
+    
+    time.sleep(5)
+
+    selected_date = calendar.find_element(By.XPATH, "//td[contains(@class, 'active day')]")
+    yesterday = datetime.today() - timedelta(days=1)
+    yesterday = yesterday.day
+
+    # Pobierz datę zaznaczonego elementu
+    if(selected_date.text == str(yesterday)):
+        # nie robimy nic jeśli jest wczorajsza data zaznaczona
+        return
+    else:
+        previous_date = calendar.find_element(By.XPATH, f"//*[ text() = '{yesterday}' ]")
+        previous_date.click()
+        # klikamy i czekamy aż załaduje tabelkę na dzień dzisiejszy
+        time.sleep(5)
+        return
+    
+
+def Save_data_csv(driver):
+    try:
+        table = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//table[@id='footable_kontrakty_godzinowe']/tbody"))
+        )
+        hours = []
+        prices = []
+        rows = table.find_elements(By.XPATH, "./tr")
+        for row in rows:
+            cells = row.find_elements(By.XPATH, "./td")
+            row_data = [cell.text for cell in cells]
+            hours.append(str(row_data[0]).split("-")[0])
+            prices.append(row_data[1])
+    except Exception as e:
+        print("Wystąpił błąd podczas scrapowania cen: ", str(e))
+
+    try:
+        prices = [x.replace(',', '.') for x in prices]
+        prices = [ float(x) for x in prices]
+        df = pd.DataFrame({'hour': hours, 'da1_price': prices})
+        df["date"] = str(datetime.today().date()) + ' ' + df["hour"].astype(str) + ':00:00.000000 UTC'
+        df.index = df["date"]
+        df.index = pd.to_datetime(df.index)
+        df.drop(['hour','date'], inplace=True, axis=1)
+        df.to_csv("data\\newest_prices.csv")
+    except Exception as e:
+        print("Wystąpił błąd podczas zapisywania CSV: ", str(e))
+    return
+
+
+def Start_price_scraping():
+    url = "https://tge.pl/energia-elektryczna-rdn"
+
+    # chyba działa bez tego, wtf XD
+    edge_driver_path = 'edge_driver\\msedgedriver.exe'
+    try:
+        driver = webdriver.Edge()
+        driver.get(url)
+    except Exception as e:
+        print("Wystąpił błąd podczas uruchamiania drivera selenium do Edge: ", str(e))
+    try:
+        Correct_date_click(driver)
+    except Exception as e:
+        print("Wystąpił błąd podczas ustawiania dobrej daty w kalendarzu: ", str(e))
+        
+    Save_data_csv(driver)
+    driver.quit()
+    return
